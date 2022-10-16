c7440b1c4cc3ba69a94c0082f09e108dd0f28484
yn0315 <yunu0315@naver.com>
2022-10-16 20:59:23 +0900

    [2022.10.16]

--- 221014.py
+++ 221014.py
@@ -193,12 +193,25 @@ print(character)
 
 limit = 10000
 i = 1
+sum_value = 0
 
-while True:
-    if i >= 10000:
-        break
-    sum_value = 0
-
+while sum_value >= 10000:
     sum_value += i
     i += 1
+
 print("{}를 더할 때 {}을 넘으며 그때의 값은 {}입니다.".format(i - 1, limit, sum_value))
+
+
+max_value = 0
+a = 0
+b = 0
+
+for i in range(1,100):
+    j = 100 -i
+    if max_value < i * j:
+        max_value = i * j
+        a = i
+        b = j
+
+
+print("최대가 되는 경우: {} * {} = {}".format(a,b,max_value))
